37f6c809220eb26af61171638736a556e4bb220f
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-06-04 17:51:02 +0900

    build: add theme.ts and styled-component.d.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+const theme = {
+  color: {
+    background: {
+      primary: "#fff",
+      secondary: "#0065c4",
+      tertiary: "#3a3a3a"
+
+    },
+    font: {
+      primary: "#fff",
+      secondary: "#000",
+      error: "#fc2626",
+    },
+  },
+  size: {
+    font: {
+      huge: "1.778rem",
+      large: "1.33rem", // 48px (36px is regular)
+      regular: "1rem",
+      small: "0.66rem", // 24px (36px is regular)
+      extraSmall: "0.4rem",
+      tiny: "0.33rem",
+    },
+  },
+  range: {
+    screen: {
+      maxWidth: "1440px",
+      mobileToTablet: "544px", // 544px by default
+      tabletToDesktop: "1104px",
+    },
+  },
+}
+
+export default theme;
