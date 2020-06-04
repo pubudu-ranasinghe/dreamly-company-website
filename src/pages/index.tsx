7f2642991a3b1a3eac00585d2d6896a8355fb87a
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-06-04 18:04:33 +0900

    build: added styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import React from "react"
+import styled, { css } from "styled-components"
 
+const Container = styled.div(
+  ({ theme }) => css`
+    color: red;
+  `
+)
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Container>Hello world!</Container>
 }
