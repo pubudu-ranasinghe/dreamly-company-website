19a69f2265483126ca97f7522dbe75acceed510f
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-06-05 14:21:12 +0900

    build: created Header.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,17 @@ import React from "react"
 import { ThemeProvider } from "styled-components"
 import theme from "../../styles/theme"
 import "../../css/style.css"
+import Header from "./Header"
 
 interface Props {}
 
 const Layout = (props: any) => {
   return (
     <>
-      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Header />
+        {props.children}
+      </ThemeProvider>
     </>
   )
 }
