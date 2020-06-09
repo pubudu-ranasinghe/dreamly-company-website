62ed9563894d89fecb2d422241ed6bfa03725519
desmukh <desmukh@users.noreply.github.com>
2020-06-09 19:19:22 +0500

    build: move body styling from style.css to GlobalStyle

--- src/components/views/Layout/Layout.tsx
+++ src/components/views/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider, createGlobalStyle } from "styled-components"
 import theme from "../../../styles/theme"
 import "../../../css/style.css"
 import Header from "./Header"
@@ -8,10 +8,25 @@ import 'typeface-varela-round';
 
 interface Props {}
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    max-width: 1680px;
+    margin: auto;
+    text-align: center;
+    height: 100%;
+    font-family: 'Varela Round',sans-serif;
+    color: #000;
+  }
+  html, #___gatsby, #gatsby-focus-wrapper {
+    height: 100%;
+  }
+`
+
 const Layout = (props: any) => {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Header />
         {props.children}
         <Footer />
