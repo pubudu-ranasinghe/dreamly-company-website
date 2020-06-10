0433aa1b6050871bd257e8db82b052c9f10f165b
desmukh <desmukh@users.noreply.github.com>
2020-06-11 01:03:25 +0500

    build: added responsive styling to cover

--- src/components/views/home/Cover.tsx
+++ src/components/views/home/Cover.tsx
@@ -4,17 +4,36 @@ import styled, { css } from "styled-components"
 import backgroundimg from '../../../assets/images/background/bc-image.png'
 import BackgroundImage from 'gatsby-background-image'
 
-const Title = styled.div`
-  color: #fff;
-  font-family: "Georgia", Sans-serif;
-  font-size: 1.4em;
-  font-weight: 600;
-  line-height: 2em;
-  letter-spacing: 4px;
-  text-align: left;
-	padding: 20% 30% 20% 5%;
-}
-`
+const Title = styled.h1(
+  ({theme}) => css`
+    color: #fff;
+    font-family: "Georgia", Sans-serif;
+    font-size: 1.2em;
+    font-weight: 600;
+    line-height: 2em;
+    letter-spacing: 4px;
+    text-align: center;
+    margin-bottom: 1.6em;
+    @media (min-width: ${theme.range.screen.mobileToTablet}) {
+      text-align: left;
+    }
+    @media (min-width: ${theme.range.screen.tabletToDesktop}) {
+      text-align: left;
+    }
+  `
+)
+
+const Wrapper = styled.div(
+  ({ theme }) => css`
+    padding: 5% 5% 10% 5%;
+    @media (min-width: ${theme.range.screen.mobileToTablet}) {
+      padding: 5% 30% 10% 5%;
+    }
+    @media (min-width: ${theme.range.screen.tabletToDesktop}) {
+      padding: 20% 30% 20% 5%;
+    }
+  `
+)
 
 const Cover = ({ className }) => (
   <StaticQuery
@@ -40,10 +59,14 @@ const Cover = ({ className }) => (
           fluid={imageData}
           backgroundColor={`#040e18`}
         >
-          <Title>
-            <h1>Dreamly is an international team bringing state of the art IT skills to Kagawa.</h1>
-            <h1>We will help invigorating your dreams, even in the rural parts of Japan.</h1>
-          </Title>
+          <Wrapper>
+            <Title>
+              Dreamly is an international team bringing state of the art IT skills to Kagawa.
+            </Title>
+            <Title>
+              We will help invigorating your dreams, even in the rural parts of Japan.
+            </Title>
+          </Wrapper>
         </BackgroundImage>
       )
     }}
@@ -52,10 +75,16 @@ const Cover = ({ className }) => (
 
 const StyledCover = styled(Cover)`
   width: 100%;
-  background-position: center center;
+  background-position: center right;
   background-repeat: repeat-y;
   background-size: cover;
-  height: 100%;
+  height: auto;
+  @media (min-width: ${props => props.theme.range.screen.mobileToTablet}) {
+
+  }
+  @media (min-width: ${props => props.theme.range.screen.tabletToDesktop}) {
+    height: 100%;
+  }
 `
 
 export default StyledCover
